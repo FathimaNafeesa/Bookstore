e07c51b1cbe77583351b7e6572377fc66f1442ba
FathimaNafeesa <fathimanafeesavy@gmail.com>
2020-07-20 18:57:01 +0530

    [fathima]Refator. response added

--- views/get_books_view.py
+++ views/get_books_view.py
@@ -4,7 +4,8 @@ from flask import (jsonify, make_response, redirect, render_template, request,
                    session, url_for)
 from flask_restful import Resource
 from werkzeug.datastructures import ImmutableMultiDict
-from werkzeug.exceptions import BadRequestKeyError
+from werkzeug.exceptions import  BadRequestKeyError
+from services.services import  sort_books,calling_book_details
 
 from app import api, app
 
@@ -13,8 +14,8 @@ class Books(Resource):
         #to sort
         try:
             sort_parameter = request.args['sort_parameter']
-
-            return make_response(jsonify({"response": "sort book list called"}), 200)
+            sorted_list = sort_books(sort_parameter)
+            return make_response(jsonify(result=sorted_list))
         except BadRequestKeyError:
             return make_response(jsonify({"response": "unsort book list called"}), 200)
 
