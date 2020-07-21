dfcab0bb561c75b1b3fcbeafe68df1430ae7908f
FathimaNafeesa <fathimanafeesavy@gmail.com>
2020-07-21 13:26:10 +0530

    [fathima]Add. search book provision added

--- views/get_books_view.py
+++ views/get_books_view.py
@@ -5,20 +5,31 @@ from flask import (jsonify, make_response, redirect, render_template, request,
 from flask_restful import Resource
 from werkzeug.datastructures import ImmutableMultiDict
 from werkzeug.exceptions import  BadRequestKeyError
-from services.services import  sort_books,calling_book_details
+from services.services import  sort_books,calling_book_details,search_books
 
 from app import api, app
+
 
 class Books(Resource):
     def get(self):
         #to sort
         try:
             sort_parameter = request.args['sort_parameter']
-            sorted_list = sort_books(sort_parameter)
-            return make_response(jsonify(result=sorted_list))
+            if sort_parameter:
+                sorted_list = sort_books(sort_parameter)
+                return make_response(jsonify(result=sorted_list))    
         except BadRequestKeyError:
             return make_response(jsonify({"response": "unsort book list called"}), 200)
 
+    def post(self):
+        try:
+            book_form = request.form
+            search_parameter = book_form['search_parameter']
+            if search_parameter:
+                search_result = search_books(search_parameter)
+                return make_response(jsonify({"books" : search_result}),200)
+        except BadRequestKeyError:
+            return make_response(jsonify({"response": "unsort book list called"}), 200)
             
 
 api.add_resource(Books, '/books')
