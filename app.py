1dc19f82d158c1c0fe0bbb8db82bd78371f04f77
FathimaNafeesa <fathimanafeesavy@gmail.com>
2020-07-21 22:09:50 +0530

    [fathima]Add. imported admin view file

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify,Blueprint
 from flask_redis import FlaskRedis
 from flask_restful import Api
+from flasgger import Swagger
+from flasgger.utils import swag_from
+from services.error_handler_service import InvalidUsageError
 
-from error_handler_service import InvalidUsageError
 
 
 app = Flask(__name__)
-api = Api(app)
+#frombookstore_blueprint = Blueprint("bookstore_blueprint", __name__)
 
+api = Api(app)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///bookstore_db.db"
 
 
 @app.errorhandler(InvalidUsageError)
@@ -20,4 +25,6 @@ def handle_invalid_usage(error):
     response.status_code = error.status_code
     return response
 
-from views import *
+from views.login_and_registration_view import *
+from views.get_books_view import *
+from views.admin_view import *
