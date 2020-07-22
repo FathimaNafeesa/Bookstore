bb3ae29cf000f8b2ad35d6e7f622b71b369e6ea7
FathimaNafeesa <fathimanafeesavy@gmail.com>
2020-07-22 11:04:47 +0530

    [fathima]Add. service for deleting book from db

--- services/admin_services.py
+++ services/admin_services.py
@@ -1,14 +1,26 @@
-from model import ProductData
+from model import ProductData,db
 from sqlalchemy import desc
 from sqlalchemy.exc import IntegrityError,OperationalError,InvalidRequestError,CompileError
 from services.error_handler_service import InvalidUsageError
 
 class AdminServices:
 
-    def add_book(id,title,author,image,quantity,price,description):
-        new_book = ProductData(id = id,title = title,author = author,image = image,quantity = quantity,price = price,description = description)
-        db.session.add(new_book)
-        de.session.commit()
-        return "Done"
+    def add_books(id,title,author,image,quantity,price,description):
+        try:
+            new_book = ProductData(id = id,title = title,author = author,image = image,quantity = quantity,price = price,description = description)
+            db.session.add(new_book)
+            db.session.commit()
+            return "Done"
+        except (InvalidRequestError,OperationalError,CompileError) :
+            raise InvalidUsageError('mysql connection or syntax is improper', 500)
 
-    
+
+    def delete_book(id):
+        try:
+            book =  ProductData.query.filter_by(id=id).delete()
+            db.session.commit()
+            return "Done"
+        except (InvalidRequestError,OperationalError,CompileError) :
+            raise InvalidUsageError('mysql connection or syntax is improper', 500)
+        
+
