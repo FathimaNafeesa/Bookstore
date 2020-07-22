757eb17882e74850e60cc488d9549061b4447403
FathimaNafeesa <fathimanafeesavy@gmail.com>
2020-07-22 15:31:46 +0530

    [fathima]Refactor. debugged admin_services

--- services/admin_services.py
+++ services/admin_services.py
@@ -1,25 +1,23 @@
-from model import ProductData,db
-from sqlalchemy import desc
-from sqlalchemy.exc import IntegrityError,OperationalError,InvalidRequestError,CompileError
+from model import ProductData, db
+from sqlalchemy.exc import IntegrityError, OperationalError, InvalidRequestError, CompileError
 from services.error_handler_service import InvalidUsageError
 
-class AdminServices:
 
-    def add_books(id,title,author,image,quantity,price,description):
-        try:
-            new_book = ProductData(id = id,title = title,author = author,image = image,quantity = quantity,price = price,description = description)
-            db.session.add(new_book)
-            db.session.commit()
-            return "Done"
-        except (InvalidRequestError,OperationalError,CompileError) :
-            raise InvalidUsageError('mysql connection or syntax is improper', 500)
+def add_books(id, title, author, image, quantity, price, description):
+    try:
+        new_book = ProductData(id=id, title=title, author=author, image=image,
+                               quantity=quantity, price=price, description=description)
+        db.session.add(new_book)
+        db.session.commit()
+        return "Done"
+    except (InvalidRequestError, OperationalError, CompileError):
+        raise InvalidUsageError('mysql connection or syntax is improper', 500)
 
 
-    def delete_book(id):
-        try:
-            book =  ProductData.query.filter_by(id=id).delete()
-            db.session.commit()
-            return "Done"
-        except (InvalidRequestError,OperationalError,CompileError) :
-            raise InvalidUsageError('mysql connection or syntax is improper', 500)
-
+def delete_book(id):
+    try:
+        book = ProductData.query.filter_by(id=id).delete()
+        db.session.commit()
+        return "Done"
+    except (InvalidRequestError, OperationalError, CompileError):
+        raise InvalidUsageError('mysql connection or syntax is improper', 500)
