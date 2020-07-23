e648c9c42dbe27d2a3aa71d2f5b915038894de1e
FathimaNafeesa <fathimanafeesavy@gmail.com>
2020-07-23 13:32:47 +0530

    [fathima]Refactor. many to many relationship established between user and product_data

--- model.py
+++ model.py
@@ -1,9 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+
+
 from app import app
 from marshmallow import Schema, fields, ValidationError, pre_load
 
 db = SQLAlchemy(app)
 
+relationship_table = db.Table('relationship_table', db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key = True),
+            db.Column('product_id', db.Integer, db.ForeignKey('product_data.id'), primary_key = True))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +16,7 @@ class User(db.Model):
     phone = db.Column(db.String(10), unique=True)
     password = db.Column(db.String(100))
     is_verified = db.Column(db.Boolean, nullable=False)
+    wishlist = db.relationship('ProductData', secondary=relationship_table, backref=db.backref('products', lazy='dynamic'))
 
 
 class ProductData(db.Model):
@@ -27,6 +32,7 @@ class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True)
     phone = db.Column(db.String(10), unique=True)
+
 
 # # SCHEMAS
 
