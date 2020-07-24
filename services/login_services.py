b8c65b6dc82ff313c606b4e2eb1975b6f3d59ede
FathimaNafeesa <fathimanafeesavy@gmail.com>
2020-07-24 15:01:58 +0530

    [fathima]Add. included service to delete from cart

--- services/login_services.py
+++ services/login_services.py
@@ -38,17 +38,21 @@ def display_cart(username):
     except (InvalidRequestError,OperationalError,CompileError) :
             raise InvalidUsageError('mysql connection or syntax is improper', 500) 
 
-def add_books_to_cart(id,username):
+def add_or_delete_books_to_cart(id,username,action)
     try:
         user = User.query.filter_by(username = username).first()
         book = ProductData.query.filter_by(id=id).first()
-        book.products_to_order.append(user)
+        if action == "add":
+            book.products_to_order.append(user)
+        if action == "delete":
+            user.car.remove(book)
         db.session.commit()
         return True
     except (InvalidRequestError,OperationalError,CompileError) :
             raise InvalidUsageError('mysql connection or syntax is improper', 500)
 
-        
+
+
 
 
 
